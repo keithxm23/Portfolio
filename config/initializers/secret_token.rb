4e31d8b48942e55b6948766218cd88d2580c9cc9
Keith Mascarenhas <keithxm23@gmail.com>
2013-09-12 06:38:50 +0000

    working refinery app with twitter-bootstrap-rails and overriden views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = 'c2cd0e3e96c2bd6976d2f1ae40fc7e44e6104770ac34ba0ad40fe681262a579f65a8ae302ca3bde0c39c46c4cb164c8bc936a0efb5eec606e48e37d9d3e09c2c'
